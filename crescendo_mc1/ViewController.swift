e54ec8caa07e2fafae4cc21a975513643c5ef55c
geraldbryan <geraldbryan9914@gmail.com>
2022-04-05 20:52:40 +0700

    Initial Commit

---
+++ crescendo_mc1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  crescendo_mc1
+//
+//  Created by Gerald Bryan on 05/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
